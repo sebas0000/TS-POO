810065ffce7f579acdb7baec7c0170561feecdc4
sebas0000 <sebastiangomezcalvario@gmail.com>
2025-07-14 12:12:09 -0600

    Ultimos pasos en POO y TS

--- ts/04.ts
+++ ts/04.ts
@@ -1,5 +1,22 @@
+//Interface propiedades y metodos que se usaran en el objeto
+interface CamisetaBase{
+    setColor(color:string):void;
+    getColor():string;
+    estampacion?(): void;
+}
+//Decorador
+function estampar(logo: string) {
+    return function (target: Function) {
+        target.prototype.estampacion = function (): void {
+            console.log('Camisa estampada con logo de: ' + logo);
+        };
+        target.prototype.logo = logo; // Agrega propiedad logo
+    };
+}
+
 //Clase (molde del objeto)
-export class Camiseta{
+@estampar('Adidas')
+export class Camiseta implements CamisetaBase{
     //Propiedades (Caracteristicas del objeto)
     constructor(
         public color : string,
@@ -8,6 +25,7 @@ export class Camiseta{
         public talla: string,
         public precio: number
     ){}
+    estampacion!: () => void;
     //Metodos (Funciones o acciones del objeto)
     public setColor(color: string):void{
         this.color = color;
@@ -17,12 +35,34 @@ export class Camiseta{
     }
 } 
 let camiseta = new Camiseta("rojo", "manga corta", "Nike", "L", 19.99);
+camiseta.estampacion?.();
+class Sudadera extends Camiseta{
+    constructor(
+        color: string,
+        modelo: string,
+        marca: string,
+        talla: string,
+        precio: number,
+        public capucha: boolean
+    ){super(color, modelo, marca, talla, precio);}
+
+    setCapucha(capucha:boolean):void{
+        this.capucha = capucha
+    }
+    getCapucha():boolean{
+        return this.capucha
+    }
+}
+
 
 console.log(camiseta)
 camiseta.setColor('Verde')
 console.log(camiseta)
 let color : string = camiseta.getColor()
 console.log(color)
+let sudadera = new Sudadera('amarillo','sin mangas','nike','L',30,true)
+sudadera.setColor('Roja')
+console.log(sudadera)
 
 /* ASI FUNCIONA SIN CONSTRUCTOR
 // var playera = new Camiseta();
